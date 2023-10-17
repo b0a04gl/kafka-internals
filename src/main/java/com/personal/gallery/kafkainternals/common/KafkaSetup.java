380b02399b0681f918d709f39750e701a5269842
balaji6666 <balaji.foo404@gmail.com>
2023-10-17 12:37:18 +0530

    added config and abstraction layer for consumer capabilities

--- src/main/java/com/personal/gallery/kafkainternals/common/KafkaSetup.java
+++ src/main/java/com/personal/gallery/kafkainternals/common/KafkaSetup.java
@@ -7,18 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Configuration
 @Lazy
 public class KafkaSetup {
-
-
     @Bean
     @Qualifier("simpleKafkaProps")
     public KafkaProperties simpleKafkaProps() throws IOException {
@@ -57,4 +58,33 @@ public class KafkaSetup {
         if (!StringUtils.isBlank(topic)) template.setDefaultTopic(topic);
         return template;
     }
+
+    @Bean
+    public DefaultKafkaConsumerFactory<String, Object> consumerFactory(@Qualifier("kafkaPropsWithSerialisation") KafkaProperties kafkaProperties) {
+        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+        consumerProperties.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> concurrentKafkaListenerContainerFactory(@Qualifier("consumerFactory") ConsumerFactory<String, Object> consumerFactory, @Qualifier("kafkaPropsWithSerialisation") KafkaProperties kafkaProperties) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(kafkaProperties.getListener().getAckMode());
+        factory.setConcurrency(kafkaProperties.getListener().getConcurrency());
+
+        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+        if (consumerProperties.containsKey("stringToJSON")
+                && consumerProperties.get("stringToJSON").equals("true")) {
+            BatchMessagingMessageConverter messageConverter =
+                    new BatchMessagingMessageConverter(new StringJsonMessageConverter());
+            factory.setMessageConverter(messageConverter);
+        }
+        if (kafkaProperties.getListener().getType().name().equalsIgnoreCase("BATCH")) {
+            factory.setBatchListener(true);
+        }
+        return factory;
+    }
+
 }
--- src/main/java/com/personal/gallery/kafkainternals/core/abstraction/ConsumerLayer.java
+++ src/main/java/com/personal/gallery/kafkainternals/core/abstraction/ConsumerLayer.java
@@ -1,4 +1,8 @@
 package com.personal.gallery.kafkainternals.core.abstraction;
 
+import com.personal.gallery.kafkainternals.avro.pojo.MatchStats;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 public interface ConsumerLayer {
+    public void listen(ConsumerRecord<String, MatchStats> record);
 }
