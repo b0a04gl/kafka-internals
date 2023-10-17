68e3dfa6acb64428bf37e7a3863cbb8d6babb41e
balaji6666 <balaji.foo404@gmail.com>
2023-10-17 12:36:48 +0530

    updated consumer config

--- src/main/java/com/personal/gallery/kafkainternals/core/abstraction/ProducerLayer.java
+++ src/main/java/com/personal/gallery/kafkainternals/core/abstraction/ProducerLayer.java
@@ -4,6 +4,7 @@ import com.personal.gallery.kafkainternals.avro.pojo.MatchStats;
 
 public interface ProducerLayer {
     void publishMessageToKafkaWithSyncAck(Object matchStats);
+
     void publishMessageToKafkaWithAsyncAck(Object matchStats);
 
     void publishMessageToKafkaWithSyncAckAndSerialize(MatchStats matchStats);
