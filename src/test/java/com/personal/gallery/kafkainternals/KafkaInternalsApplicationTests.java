0afced099a864ba248835d45eef858c7fab59f91
balaji6666 <balaji.foo404@gmail.com>
2023-10-17 12:44:30 +0530

    added TCs for config loading

--- src/test/java/com/personal/gallery/kafkainternals/KafkaInternalsApplicationTests.java
+++ src/test/java/com/personal/gallery/kafkainternals/KafkaInternalsApplicationTests.java
@@ -1,13 +1,30 @@
 package com.personal.gallery.kafkainternals;
 
+import com.personal.gallery.kafkainternals.common.KafkaSetup;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class KafkaInternalsApplicationTests {
 
+	@Autowired
+	private KafkaSetup kafkaSetup;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		assertNotNull(kafkaSetup.simpleKafkaProps());
+		assertNotNull(kafkaSetup.kafkaPropsWithSerialisation());
+
+		assertNotNull(kafkaSetup.consumerFactory(kafkaSetup.simpleKafkaProps()));
+		assertNotNull(kafkaSetup.producerFactoryString(kafkaSetup.simpleKafkaProps()));
+
+		assertNotNull(kafkaSetup.consumerFactory(kafkaSetup.kafkaPropsWithSerialisation()));
+		assertNotNull(kafkaSetup.producerFactoryString(kafkaSetup.kafkaPropsWithSerialisation()));
 	}
 
 }
